3dbe29da91b7dd6a8e7dedf2526cbf38579654f5
mgv <mgvilarrasa@gmail.com>
2023-04-17 11:50:53 +0200

    change to export war file

--- src/main/java/com/mgv/libraryserver/LibraryServerApplication.java
+++ src/main/java/com/mgv/libraryserver/LibraryServerApplication.java
@@ -2,9 +2,16 @@ package com.mgv.libraryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryServerApplication {
+public class LibraryServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibraryServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryServerApplication.class, args);
